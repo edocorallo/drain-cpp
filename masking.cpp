d50cc8f6e2722e4828ca06e06ea0d7fe354eef6a
edocorallo <edoardo.corallo@gmail.com>
2022-08-19 22:29:05 +0200

    nothing

--- masking.cpp
+++ masking.cpp
@@ -5,14 +5,14 @@
 
 LogMasker::LogMasker(){//This below is ugly as fuck, but i can't care less about tidiness right now
     MaskingInstructions.reserve(10);
-   this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)([a-zA-Z-0-9./]+(=[a-zA-Z-0-9'./@ :]+)+(, \\.)?)((?=[^A-Za-z0-9])|$)"), std::string("<ID>")));//For StoRM-ATLAS FE
+    this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)([a-zA-Z-0-9./]+(=[a-zA-Z-0-9'./@ :]+)+(, \\.)?)((?=[^A-Za-z0-9])|$)"), std::string("<ID>")));//For StoRM-ATLAS FE
     this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)((:*)([a-f0-9]{1,4}(\\.|:+)){3,7}[a-f0-9]{1,4})((?=[^A-Za-z0-9])|$)"), std::string("<IP>")));//For Messed up IPs at ATLAS
     this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)(\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12})((?=[^A-Za-z0-9])|$)"), std::string("<TKN>")));//For StoRM-ATLAS FE
     this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)\\w+]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:[0-9a-fA-F][0-9a-fA-F]))+((?=[^A-Za-z0-9])|$)"), std::string("<URL>")));
     this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)(0x[a-f0-9A-F]+)((?=[^A-Za-z0-9])|$)"), std::string("<HEX>")));//hexadecimal
     this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)'([\\-\\+]?\\d+)'((?=[^A-Za-z0-9])|$)"), std::string("<NUM>")));//Numbers
-        this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)'([a-zA-Z-0-9./]+(=[a-zA-Z-0-9./@ :]+)+(, )?)'((?=[^A-Za-z0-9])|$)"), std::string("<ID>")));//Identifiers
- this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)('[A-Za-z\\s]+')((?=[^A-Za-z0-9])|$)"), std::string("<TSK>")));//Task
+    this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)'([a-zA-Z-0-9./]+(=[a-zA-Z-0-9./@ :]+)+(, )?)'((?=[^A-Za-z0-9])|$)"), std::string("<ID>")));//Identifiers
+    this->MaskingInstructions.push_back(std::make_pair(std::regex("((?<=[^A-Za-z0-9])|^)('[A-Za-z\\s]+')((?=[^A-Za-z0-9])|$)"), std::string("<TSK>")));//Task
 };
 
 std::string LogMasker::mask(std::string & content){
